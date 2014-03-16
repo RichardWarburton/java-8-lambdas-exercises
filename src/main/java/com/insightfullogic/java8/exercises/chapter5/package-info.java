be17bf59edc60242791db925911501616709f3ca
Richard Warburton <richard.warburton@gmail.com>
2014-03-16 21:00:29 +0000

    tidy chapter 5 examples up

---
+++ src/main/java/com/insightfullogic/java8/exercises/chapter5/package-info.java
@@ -0,0 +1,15 @@
+package com.insightfullogic.java8.exercises.chapter5;
+
+/**
+ * Question 1:
+ *  TODO
+ *
+ * Question 2:
+ *  a. TODO
+ *  b. See WordCount
+ *  c. See GroupingBy
+ *
+ * Question 3:
+ *  See Fibonacci
+ *
+ */
