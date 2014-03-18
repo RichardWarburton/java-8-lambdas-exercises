28069f2dc5a93d46af45a2ce0c88b90a2357b3f0
Richard Warburton <richard.warburton@gmail.com>
2014-03-18 16:11:55 +0000

    add missing exercise - chapter 3, question 1

--- src/main/java/com/insightfullogic/java8/examples/chapter1/SampleData.java
+++ src/main/java/com/insightfullogic/java8/examples/chapter1/SampleData.java
@@ -23,6 +23,8 @@ public class SampleData {
 
     public static final Album sampleShortAlbum = new Album("sample Short Album", asList(new Track("short track", 30)), asList(johnColtrane));
 
+    public static final Album manyTrackAlbum = new Album("sample Short Album", asList(new Track("short track", 30), new Track("short track 2", 30), new Track("short track 3", 30), new Track("short track 4", 30), new Track("short track 5", 30)), asList(johnColtrane));
+
     public static Stream<Album> albums = Stream.of(aLoveSupreme);
 
     public static Stream<Artist> threeArtists() {
--- src/test/java/com/insightfullogic/java8/exercises/chapter3/Question1Test.java
+++ src/test/java/com/insightfullogic/java8/exercises/chapter3/Question1Test.java
@@ -1,9 +1,17 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
+import com.insightfullogic.java8.examples.chapter1.Album;
+import com.insightfullogic.java8.examples.chapter1.SampleData;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
+import static com.insightfullogic.java8.examples.chapter1.SampleData.aLoveSupreme;
+import static com.insightfullogic.java8.examples.chapter1.SampleData.manyTrackAlbum;
+import static com.insightfullogic.java8.examples.chapter1.SampleData.sampleShortAlbum;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 public class Question1Test {
@@ -22,7 +30,15 @@ public class Question1Test {
 
     @Test
     public void extractsNamesAndOriginsOfArtists() {
+        List<String> namesAndOrigins = Question1.getNamesAndOrigins(SampleData.getThreeArtists());
+        assertEquals(asList("John Coltrane", "US", "John Lennon", "UK", "The Beatles", "UK"), namesAndOrigins);
+    }
 
+    @Test
+    public void findsShortAlbums() {
+        List<Album> input = asList(manyTrackAlbum, sampleShortAlbum, aLoveSupreme);
+        List<Album> result = Question1.getAlbumsWithAtMostThreeTracks(input);
+        assertEquals(asList(sampleShortAlbum, aLoveSupreme), result);
     }
 
 }
