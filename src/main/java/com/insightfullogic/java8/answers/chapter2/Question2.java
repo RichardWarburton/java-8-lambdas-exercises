4f3d63697dbb37db76b01f5f3ec738546c582b08
Richard Warburton <richard.warburton@gmail.com>
2014-09-02 16:12:38 +0100

    revert broken fix from pull request

--- src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
@@ -1,10 +1,12 @@
 package com.insightfullogic.java8.answers.chapter2;
 
 import java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;
 
 import static java.lang.ThreadLocal.withInitial;
 
 public class Question2 {
 
-     public final static ThreadLocal<DateFormatter> formatter = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")) );
+     public final static ThreadLocal<SimpleDateFormat> formatter = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd-MMM-yyyy") );
+
 }
