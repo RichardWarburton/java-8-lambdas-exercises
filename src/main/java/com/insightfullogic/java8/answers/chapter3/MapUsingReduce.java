6c3e4c7afa0995ad5e45d6f480a8a956289da669
Algirdas Rascius <arascius@kayak.com>
2014-09-29 11:15:13 +0300

    Fixed MapUsingReduce
    Contract of Stream.reduce method requires that accumulator function
    does not mutate its arguments. Previous implementation of
    MapUsingReduce.map method was using accumulator and combiner
    functions that were mutating list passed as argument. Mutating
    parameters in accumulator and combiner functions results in
    incorrect behaviour when parallel stream is processed.
    
    New implementation of MapUsingReduce.map copies argument lists
    in accumulator and combiner functions. This is very inefficient.
    Stream.collect method could be used to implement more efficient
    mutable reduction, but exercise 1 of chapter 3 asks to use reduce
    method explicitly.
    
    Unit test MapUsingReduceTest was updated to check functionality of
    MapUsingReduce.map with parallel streams.

--- src/main/java/com/insightfullogic/java8/answers/chapter3/MapUsingReduce.java
+++ src/main/java/com/insightfullogic/java8/answers/chapter3/MapUsingReduce.java
@@ -12,11 +12,19 @@ public class MapUsingReduce {
 
     public static <I, O> List<O> map(Stream<I> stream, Function<I, O> mapper) {
         return stream.reduce(new ArrayList<O>(), (acc, x) -> {
-            acc.add(mapper.apply(x));
-            return acc;
+        	// We are copying data from acc to new list instance. It is very inefficient,
+        	// but contract of Stream.reduce method requires that accumulator function does
+        	// not mutate its arguments.
+        	// Stream.collect method could be used to implement more efficient mutable reduction,
+        	// but this exercise asks to use reduce method.
+        	List<O> newAcc = new ArrayList<>(acc);
+        	newAcc.add(mapper.apply(x));
+            return newAcc;
         }, (List<O> left, List<O> right) -> {
-            left.addAll(right);
-            return left;
+        	// We are copying left to new list to avoid mutating it. 
+        	List<O> newLeft = new ArrayList<>(left);
+        	newLeft.addAll(right);
+            return newLeft;
         });
     }
 
--- src/test/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduceTest.java
+++ src/test/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduceTest.java
@@ -1,13 +1,13 @@
 package com.insightfullogic.java8.exercises.chapter3;
 
-import org.junit.Test;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
-import static java.util.Arrays.asList;
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class MapUsingReduceTest {
 
@@ -29,6 +29,9 @@ public class MapUsingReduceTest {
     private <I, O> void assertMapped(Function<I, O> mapper, List<I> input, List<O> expectedOutput) {
         List<O> output = MapUsingReduce.map(input.stream(), mapper);
         assertEquals(expectedOutput, output);
+
+        List<O> parallelOutput = MapUsingReduce.map(input.parallelStream(), mapper);
+        assertEquals(expectedOutput, parallelOutput);
     }
 
 }
