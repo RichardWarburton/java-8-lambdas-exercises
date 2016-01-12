9d9712304d0bab9c310e6589a7b90528d56b0f2b
consman01 <bob.dev.oak@gmail.com>
2016-01-12 06:28:15 -0800

    The exercise is supposed to use a Lambda expression that returns a DateFormatter, but only returns a SimpleDateFormat. Now this is fixed and it returns a DateFormatter.

--- src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
@@ -3,14 +3,12 @@ package com.insightfullogic.java8.exercises.chapter2;
 import com.insightfullogic.java8.exercises.Exercises;
 
 import javax.swing.text.DateFormatter;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 
 import static java.lang.ThreadLocal.withInitial;
 
 public class Question2 {
 
-    public static ThreadLocal<SimpleDateFormat> formatter
+    public static ThreadLocal<DateFormatter> formatter
             = Exercises.replaceThisWithSolution();
 
 }
