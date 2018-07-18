bb5366179e250d37be1c139b0ea7acdc6fc8ff4d
Manny <egalli64@gmail.com>
2018-07-18 10:32:59 +0200

    http://thisthread.blogspot.com/2018/07/a-thread-safe-dateformatter-via.html
    A thread-safe DateFormatter via ThreadLocal

--- src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
@@ -1,14 +1,25 @@
+/**
+ * A thread-safe DateFormatter via ThreadLocal
+ * 
+ * @author manny egalli64@gmail.com
+ * based on Java 8 Lambdas, Richard Warburton - exercise 2-b, chapter two
+ * 
+ * @see http://thisthread.blogspot.com/2018/07/a-thread-safe-dateformatter-via.html
+ *      https://github.com/RichardWarburton/java-8-lambdas-exercises
+ */
 package com.insightfullogic.java8.exercises.chapter2;
-
-import com.insightfullogic.java8.exercises.Exercises;
 
 import javax.swing.text.DateFormatter;
 
-import static java.lang.ThreadLocal.withInitial;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Question2 {
+    public final static ThreadLocal<DateFormatter> formatter = //
+            ThreadLocal.withInitial(() -> new DateFormatter( //
+                    new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH)));
 
-    public static ThreadLocal<DateFormatter> formatter
-            = Exercises.replaceThisWithSolution();
-
+    public final static DateTimeFormatter formatter8 = //
+            DateTimeFormatter.ofPattern("dd-MMM-yyyy", Locale.ENGLISH);
 }
