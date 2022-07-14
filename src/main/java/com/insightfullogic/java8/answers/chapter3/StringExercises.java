b7137f76873e3d78c9cb40bdbc3473ea37582a5e
ConnieSookie <milliontechconniel@gmail.com>
2022-07-14 15:51:33 +0100

    Update StringExercises.java
    Handle the string is null value.

--- src/main/java/com/insightfullogic/java8/answers/chapter3/StringExercises.java
+++ src/main/java/com/insightfullogic/java8/answers/chapter3/StringExercises.java
@@ -2,12 +2,14 @@ package com.insightfullogic.java8.answers.chapter3;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class StringExercises {
 
     // Question 6
     public static int countLowercaseLetters(String string) {
+        if (Objects.isNull(string)) return 0;
         return (int) string.chars()
                            .filter(Character::isLowerCase)
                            .count();
